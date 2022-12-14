435c3e20333121ddb0454f6b2e13c550f9239c52
Elior Yousefi <elioryous@gmail.com>
2022-12-14 12:25:17 +0200

    added graphics to project

---
+++ chessGraphics/chessGraphics/mainForm.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace chessGraphics
+{
+    public partial class mainForm : Form
+    {
+        public mainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form1 gameForm = new Form1();
+            gameForm.Show();
+
+            this.Hide();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
