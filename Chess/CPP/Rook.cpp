0516ce25ef79a53035b9f891958e89fce5d9bcc6
yaniv yehezkel <yanivye106@gmail.com>
2022-12-17 16:31:59 +0200

    added rook valid move

--- Chess/CPP/Rook.cpp
+++ Chess/CPP/Rook.cpp
@@ -6,5 +6,10 @@ Rook::Rook(string type, string placement, Player player) : Piece(type, placement
 
 bool Rook::isValidMove(string dest) const
 {
+    if (dest[0] == this->_placement[0] || dest[1] == this->_placement[1]) 
+    {
+        return true;
+    }
+
     return false;
 }
