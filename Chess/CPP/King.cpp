4f891c9ee9ab1f1c485349d38ce4a0c8e0a59f03
yaniv yehezkel <yanivye106@gmail.com>
2022-12-17 16:24:28 +0200

    added king valid move

--- Chess/CPP/King.cpp
+++ Chess/CPP/King.cpp
@@ -7,5 +7,12 @@ King::King(string type, string placement, Player player) : Piece(type, placement
 
 bool King::isValidMove(string dest) const
 {
-    return false;
+    int row_difference = abs(dest[1] - this->_placement[1]);
+    int col_difference = abs(dest[0] - this->_placement[0]);
+    if (row_difference > 1 || col_difference > 1) 
+    {
+        return false;
+    }
+
+    return true;
 }
