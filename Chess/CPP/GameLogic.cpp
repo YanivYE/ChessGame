286c81155c596b7964c9eb2efc9253537e092f65
yaniv yehezkel <yanivye106@gmail.com>
2022-12-31 16:14:54 +0200

    finished comments

--- Chess/CPP/GameLogic.cpp
+++ Chess/CPP/GameLogic.cpp
@@ -1,9 +1,6 @@
 #include "../Headers/GameLogic.h"
 
 
-// TODO: bonuses - adding checkmate, castling, multiplayer option, time limit?
-
-
 /*
 * Function is the constructor for the game logic object, it gets the graphic board string
 * Input: graphicBoard - the initial board arangment string
@@ -180,7 +177,7 @@ int GameLogic::movementCode(const string source, const string destination, vecto
 }
 
 /*
-* Function gets the source, dest placements, and returns if the source and dest placements are the same.
+* Function checks code 7 case. gets the source, dest placements, and returns if the source and dest placements are the same.
 * Input: source - the source placement
 *		 destination - the destination placement
 * Output: if the source and dest placements are the same
@@ -192,7 +189,7 @@ bool GameLogic::checkCode7(const string source, const string destination) const
 }
 
 /*
-* Function gets the source player, dest placement, and returns if the source player to move doesn't
+* Function checks code 2 case. gets the source player, dest placement, and returns if the source player to move doesn't
 * have piece of current player.
 * Input: sourcePlayer - the source placement
 *		 currentPlayer - the destination placement
@@ -205,7 +202,7 @@ bool GameLogic::checkCode2(const Player sourcePlayer, const Player currentPlayer
 }
 
 /*
-* Function gets the dest, current player colors and returns if the dest player
+* Function checks code 3 case. gets the dest, current player colors and returns if the dest player
 * piece has current player piece.
 * Input: destPlayer - the source placement
 *		 currentPlayer - the destination placement
@@ -217,7 +214,7 @@ bool GameLogic::checkCode3(const Player destPlayer, const Player currentPlayer) 
 }
 
 /*
-* Function gets the source, dest pieces, a vector of the board, and return the if the moving
+* Function checks code 6 case. gets the source, dest pieces, a vector of the board, and return the if the moving
 * src to dest is valid move.
 * Input: srcP - the source placement
 *		 destP - the destination placement
@@ -231,12 +228,13 @@ bool GameLogic::checkCode6(const Piece* srcP, const Piece* destP, vector<Piece*>
 }
 
 /*
-* Function gets the source, dest placement, the current player, a vector of the board, and return
-* if check will accure on player because he moved a piece.
+* Function checks code 4 case - if chess will accure on player because he moved a piece. it just uses the 
+* madeChess function following this move and operating it on the current player that made this move
 * Input: source - the source placement
 *		 destination - the destination placement
 *		 currentPlayer - the current player
 *		 board - the vector of the board
+*		 checkingKingEscape - is this made chess function cheking king escape from chess case
 * Output: if check will accure on player because he moved a piece
 */
 bool GameLogic::checkCode4(const string source, const string destination, const Player currentPlayer, 
@@ -246,27 +244,31 @@ bool GameLogic::checkCode4(const string source, const string destination, const 
 }
 
 /*
-* Function gets the current player color, the destiantion placement, a vector of the board and returns 
-* if check will accure on openent if we move a piece to the destiantion.
-* Input: currentPlayer - the current player color
-*		 destination - the destination placement 
-		 board - the vector of the board
-* Output: if check will accure on openent if we move a piece to the destiantion
+* Function checks code 1 case - if chess will accure on oppenent player because this player moved a piece. it just uses 
+* the madeChess function following this move and operating it on the opponent player that will be in threat.
+* it also changes the opponent king inCheck field to true 
+* Input: source - the source placement
+*		 destination - the destination placement
+*		 opponentPlayer - the opponent player
+*		 board - the vector of the board
+*		 checkingKingEscape - is this made chess function cheking king escape from chess case
+* Output: if check will accure on opponent because we moved a piece
 */
-bool GameLogic::checkCode1(const string source, const string destination, const Player currentPlayer,
+bool GameLogic::checkCode1(const string source, const string destination, const Player opponentPlayer,
 	vector<Piece*>& board, bool checkingKingEscape) 
 {
 	bool isCheck = false;
-	// get openent king
+
 	Piece* opponentKing = nullptr;
 
 	// after we already moved the player, we check if the destination piece can make move on king,
 	// if so, so it's threatening the openents king
-	isCheck = madeChess(source, destination, opponentColor(currentPlayer), board, checkingKingEscape);
+	isCheck = madeChess(source, destination, opponentPlayer, board, checkingKingEscape);
 
-	opponentKing = currPlayerKing(opponentColor(currentPlayer), board);
+	// get openent king
+	opponentKing = currPlayerKing(opponentPlayer, board);
 
-	if (isCheck)
+	if (isCheck)	// if the opponent king is in check
 	{
 		((King*)opponentKing)->_inCheck = true;
 	}
@@ -275,29 +277,59 @@ bool GameLogic::checkCode1(const string source, const string destination, const 
 		((King*)opponentKing)->_inCheck = false;
 	}
 
+	// return if we made check or not
 	return isCheck;
 }
 
+/*
+* Function checks code 8 case - if checkmate will accure on oppenent player because this player moved a piece. 
+* it checks all checkmate conditions: the king is in check, he has no escape from the threat, it is not possible to 
+* capture the attacker and it is not possible to interpose in the path between the attacker and the king
+* Input: opponentPlayer - the opponent player
+*		 board - the vector of the board
+*		 attacker - the attacking piece
+* Output: if checkmate will accure on opponent because we moved a piece
+*/
 bool GameLogic::checkCode8(const Player opponentPlayer, const Piece* attacker, vector<Piece*>& board)
 {
-	Piece* opponentKing = currPlayerKing(opponentPlayer, board);
-	Piece* copiedAttacker = BoardManager::copyPiece(attacker);
-	Piece* copiedKing = BoardManager::copyPiece(opponentKing);
+	Piece* opponentKing = currPlayerKing(opponentPlayer, board);	// get opponent's king
+	Piece* copiedAttacker = BoardManager::copyPiece(attacker);	// get copied attacker piece
+	Piece* copiedKing = BoardManager::copyPiece(opponentKing);	// get copied opponent's king piece
 	if (isPossibleKingEscape(opponentKing, opponentPlayer, board)||
 		isPossibleSquareCapture(copiedAttacker->_placement, opponentPlayer, board) ||
 		isPossibleInterpose(copiedAttacker, copiedKing, board))
+		// check codeitions
 	{
-		return false;
+		return false;	// invalid checkmate
 	}
-	return true;
+	return true;	// made checkmate
 }
 
-
+/*
+* Function checks code 9 case - if player made castling on his king.
+* it checks if the source piece is a king and if made valid castling
+* Input: srcP - the source piece
+*		 destP - the destination piece
+*		 board - the pieces vector of the board	
+* Output: if made valid castling on king
+*/
 bool GameLogic::checkCode9(const Piece* srcP, const Piece* destP, const vector<Piece*> board) const
 {
 	return srcP->_type == KING && King::isCastling(destP->_placement, board, srcP);
 }
 
+/*
+* Function checks if chess will accure on given player because of moved a piece. 
+* it creates a copy of the current board- before performing this move, and changes the original board by the move 
+* that was made. then it checks if there is any opponent piece that has a valid move to the current king, which means 
+* he is in chess position
+* Input: source - the source placement
+*		 destination - the destination placement
+*		 currentPlayer - the current player
+*		 board - the vector of the board
+*		 checkingKingEscape - is cheking king escape from chess case
+* Output: if check will accure on given player following a moved piece
+*/
 bool GameLogic::madeChess(const string source, const string destination, const Player currentPlayer,
 	vector<Piece*>& board, bool checkingKingEscape)
 {
@@ -347,60 +379,91 @@ bool GameLogic::madeChess(const string source, const string destination, const P
 	return isCheck;
 }
 
+/*
+* Function checks if the given king has any possible escape from a chess threat
+* it creates a vector of all possible king moves(one square around him), and checks if he has a valid move to one of them
+* that hasnt caused chess too
+* Input: king - the given king to check his escape
+*		 currentPlayer - the current player
+*		 board - the vector of the board
+* Output: is there a possible square the king can escape to
+*/
 bool GameLogic::isPossibleKingEscape(Piece* king, const Player currentPlayer, vector<Piece*>& board)
 {
 	int i = 0;
-	Piece* copiedKing = BoardManager::copyPiece(king);
-	vector<string> possibleMoves = ((King*)king)->getKingMoves(king->_placement);
+	Piece* copiedKing = BoardManager::copyPiece(king);	// copied king
+	vector<string> possibleMoves = ((King*)king)->getKingMoves(king->_placement);	// possible king moves vector
 
-	for (i = 0; i < possibleMoves.size(); i++)
+	for (i = 0; i < possibleMoves.size(); i++)	// running on the vector 
 	{
 		if (king->isValidMove(possibleMoves[i], board) && !checkCode3(board[placementToIndex(possibleMoves[i])]->_color, currentPlayer))
+			// checks if the king has a valid move to the current square and it is not taken by own pieces
 		{
 			if (!madeChess(king->_placement, possibleMoves[i], currentPlayer, board, true))
+				// checks if move hasnt made chess
 			{
-				king = copiedKing;
+				king = copiedKing;	// return original king
 				return true;
 			}
-			king = copiedKing;
+			king = copiedKing;	// return original king
 		}
 	}
+	// no possible escape
 	return false;
 }
 
+/*
+* Function checks if there is any opponent player that has a valid move to the given placement and hasn't made chess 
+* it is used to check if there is a possible way to cpture the attacking piece that is threatning the king
+* Input: destPlacement - the given destiantion
+*		 currentPlayer - the current player
+*		 board - the vector of the board
+* Output: is there a possible valid square capture
+*/
 bool GameLogic::isPossibleSquareCapture(const string destPlacement, const Player oppenentPlayer, vector<Piece*>& board)
 {
 	int i = 0;
 	for (i = 0; i < CHESS_BOARD_SIZE; i++)
 	{
-		if (board[i]->_color == oppenentPlayer)
+		if (board[i]->_color == oppenentPlayer)	// check every opponent player piece
 		{
-			if (board[i]->isValidMove(destPlacement, board))
+			if (board[i]->isValidMove(destPlacement, board))	// if 8the piece has a valid move to destination
 			{
-				if (!madeChess(board[i]->_placement, destPlacement, oppenentPlayer, board, true))
+				if (!madeChess(board[i]->_placement, destPlacement, oppenentPlayer, board, true))	
+					// checks if move hasnt made chess
 				{
 					return true;
 				}
 			}
 		}
 	}
+	// no possible capture
 	return false;
 }
 
+/*
+* Function checks if the there is any possible interpose between the king and the attacking piece - to prevent chess threat
+* it checks every square between the 2 pieces and findes out if any current player piece can capture it 
+* Input: attacker - the attacking piece
+*		 king - the current player king - undes chess
+*		 board - the vector of the board
+* Output: is there a possible interpose to prevent chess
+*/
 bool GameLogic::isPossibleInterpose(const Piece* attacker, const Piece* king, vector<Piece*>& board)
 {
 	// set two "points" of dest and current position like 2D array indexes
 	int curPos[2] = { CHESS_BOARD_SIDE - (attacker->_placement[1] - '1') - 1, attacker->_placement[0] - 'a' };
 	int destPos[2] = { CHESS_BOARD_SIDE - (king->_placement[1] - '1') - 1, king->_placement[0] - 'a' };
 	string boardPlacement = "";
+
 	if (attacker->_type == ROOK || attacker->_type == QUEEN)
 		// if the piece is rook or queen - checking horizontal and vertical moves
 	{
 		if (curPos[1] == destPos[1])    // if the cols are the same - moving verticaly
 		{
-			for (int i = std::min(curPos[0], destPos[0]) + 1; i < std::max(curPos[0], destPos[0]); i++)
+			for (int i = std::min(curPos[0], destPos[0]) + 1; i < std::max(curPos[0], destPos[0]); i++) // checking every square
 			{
-				boardPlacement = string(1, curPos[1] + 'a') + std::to_string(CHESS_BOARD_SIDE - i);
+				boardPlacement = string(1, curPos[1] + 'a') + std::to_string(CHESS_BOARD_SIDE - i);	// square board placement
 				if (isPossibleSquareCapture(boardPlacement, king->_color, board))
 				{
 					return true;
@@ -411,13 +474,15 @@ bool GameLogic::isPossibleInterpose(const Piece* attacker, const Piece* king, ve
 		}
 		else if (curPos[0] == destPos[0])   // if the rows are the same - moving horizontaly
 		{
-			for (int i = std::min(curPos[1], destPos[1]) + 1; i < std::max(curPos[1], destPos[1]); i++)
+			for (int i = std::min(curPos[1], destPos[1]) + 1; i < std::max(curPos[1], destPos[1]); i++) // checking every square
 			{
-				boardPlacement = string(1, i + 'a') + std::to_string(CHESS_BOARD_SIDE - curPos[0]);
+				boardPlacement = string(1, i + 'a') + std::to_string(CHESS_BOARD_SIDE - curPos[0]);		// square board placement
 				if (isPossibleSquareCapture(boardPlacement, king->_color, board))
 				{
 					return true;
 				}
+				// every iteration gets a square between. check on the 
+				// square if any of the king color players can move there
 			}
 		}
 	}
@@ -436,19 +501,23 @@ bool GameLogic::isPossibleInterpose(const Piece* attacker, const Piece* king, ve
 			int col = curPos[1] + colDir;
 			// row and col is the current placment in the diagnol we check
 
-			while (row != destPos[0] && col != destPos[1])  // as long as we dont reach the diagnol end
+			while (row != destPos[0] && col != destPos[1])  // as long as we dont reach the diagnol end - checking every square
 			{
-				boardPlacement = string(1, col + 'a') + std::to_string(CHESS_BOARD_SIDE - row);
+				boardPlacement = string(1, col + 'a') + std::to_string(CHESS_BOARD_SIDE - row);		// square board placement
 				if (isPossibleSquareCapture(boardPlacement, king->_color, board))
 				{
 					return true;
 				}
+				// every iteration gets a square between. check on the 
+				// square if any of the king color players can move there
+				
 				// set next index in diangol
 				row += rowDir;
 				col += colDir;
 			}
 		}
 	}
+	// no possible interpose
 	return false;
 }
 
@@ -556,6 +625,7 @@ int GameLogic::checkCodes(const Piece* srcP, Piece* destP, vector<Piece*>& board
 		return INVALID_MOVE_ILLEGAL_MOVMENT;
 	}
 
+	// copy pieces
 	initialSrc = BoardManager::copyPiece(srcP);
 	initialDst = BoardManager::copyPiece(destP);
 
@@ -571,10 +641,12 @@ int GameLogic::checkCodes(const Piece* srcP, Piece* destP, vector<Piece*>& board
 	destP = board[destPieceIndex];
 
 	// check if valid move, and made chess
-	if (checkCode1(srcP->_placement, destP->_placement, this->_turn, board, false))
+	if (checkCode1(srcP->_placement, destP->_placement, opponentColor(this->_turn), board, false))
 	{
 		// get dest p because we changed it after check code 1
 		destP = board[destPieceIndex];
+
+		// check if valid move, and made checkmate
 		if (checkCode8(opponentColor(this->_turn), destP, board))
 		{
 			switchTurnAndClearMemory(initialSrc, initialDst);
@@ -585,6 +657,7 @@ int GameLogic::checkCodes(const Piece* srcP, Piece* destP, vector<Piece*>& board
 		return VALID_MOVE_MADE_CHESS;
 	}
 
+	// check if valid move, and made castling
 	if (checkCode9(initialSrc, initialDst, board))
 	{
 		switchTurnAndClearMemory(initialSrc, initialDst);
@@ -592,10 +665,17 @@ int GameLogic::checkCodes(const Piece* srcP, Piece* destP, vector<Piece*>& board
 	}
 
 	switchTurnAndClearMemory(initialSrc, initialDst);
+
 	// good move(code 0)
 	return VALID_MOVE;
 }
 
+/*
+* Function switches the game turn after a valid move and deletes the copied pieces
+* Input: initialSrc - the source piece
+*		 initialDst - the dest piece
+* Output: none
+*/
 void GameLogic::switchTurnAndClearMemory(Piece* initialSrc, Piece* initialDst)
 {
 	// change turns
